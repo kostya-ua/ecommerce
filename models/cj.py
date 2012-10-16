045913e9fc8088d98c34c09a8666609d33061548
Mykhailo Gendin <mishkiss@gmail.com>
2012-10-16 17:42:46 +0300

    cj link

--- models/cj.py
+++ models/cj.py
@@ -1,18 +1,26 @@
-from sqlalchemy import Column, Integer, String, Numeric, Boolean, Table, ForeignKey
+from sqlalchemy import Column, Integer, String, Numeric, Boolean, Table, ForeignKey, Text, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+import sqlalchemy.types as types
+
+import constants
+
+class ChoiceType(types.TypeDecorator):
+
+    impl = types.Integer
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(ChoiceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.iteritems() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
 
 Base = declarative_base()
 
-advertiser_link_types = Table('cj_advertiser_link_types', Base.metadata,
-                                Column('advertiser_id', Integer, ForeignKey('cj_advertisers.id')),
-                                Column('link_type_id', Integer, ForeignKey('cj_link_types.id')))
-
-class CJLinkType(Base):
-    __tablename__ = 'cj_link_types'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
 
 class CJAdvertiser(Base):
     __tablename__ = 'cj_advertisers'
@@ -31,9 +39,32 @@ class CJAdvertiser(Base):
     network_rank = Column(Integer)
     performance_incentives = Column(Boolean)
 
-    link_types = relationship('CJLinkType', secondary=advertiser_link_types)
     #HERE be actions and categories
 
     def __repr__(self):
         return self.advertiser_name
 
+class CJLink(Base):
+    __tablename__ = 'cj_links'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    link_id = Column(String)
+    advertiser_id = Column(Integer, ForeignKey('cj_advertisers.id'))
+    link_name = Column(String)
+    link_type_id = Column(ChoiceType(constants.CJ_LINK_TYPES))
+    link_destination = Column(String)
+    link_code_html = Column(Text)
+    link_code_javascript = Column(Text)
+    click_comission = Column(Numeric)
+    lead_comission = Column(Numeric)
+    sale_comission = Column(Numeric)
+    promotion_type = Column(ChoiceType(constants.CJ_PROMOTION_TYPES))
+    promotion_start_date = Column(Date)
+    promotion_end_date = Column(Date)
+    seven_day_epc = Column(Numeric)
+    three_month_epc = Column(Numeric)
+    creative_height = Column(Integer)
+    creative_width = Column(Integer)
+
+    def __repr__(self):
+        return self.link_name
