de6f1db9403872b0fc46c8debe64f9ac802584d0
Mykhailo Gendin <mishkiss@gmail.com>
2012-10-18 14:23:15 +0300

    cj fetch

--- harvester/cj.py
+++ harvester/cj.py
@@ -7,23 +7,20 @@ from top_secret import CJ_DEV_KEY, CJ_WEB_ID
 class HarvesterError(Exception):
     pass
 
-def fetch_merchants(params=None):
-    params = (isinstance(params, dict) and params) or {'keywords': 'video'}
+def fetch_cj_data(params, url, item):
     params['records-per-page'] = params.get('records-per-page', 100)
-    params['page-number'] = params.get('page-number', 0)
+    params['page-number'] = params.get('page-number', 1) - 1
     total_matched = 1
-    merchants = list()
+    data = list()
     while params['records-per-page']*params['page-number']<total_matched:
         params['page-number'] += 1
-        url = 'https://advertiser-lookup.api.cj.com/v3/advertiser-lookup?{}'.format(urlencode(params))
-        print url
-        request = Request(url, headers={'authorization': CJ_DEV_KEY})
+        request = Request('{link}?{args}'.format(link=url, args=urlencode(params)), headers={'authorization': CJ_DEV_KEY})
         try:
             xml_response = urlopen(request)
         except URLError, e:
-            raise HarvesterError('Failed to fetch merchants from CJ: {}'.format(str(e)))
+            raise HarvesterError('Failed to fetch {}s from CJ: {}'.format(item, str(e)))
 
-        response = xmltodict.parse(xml_response).get('cj-api', {}).get('advertisers', {})
+        response = xmltodict.parse(xml_response).get('cj-api', {}).get('{}s'.format(item))
         if not response:
             raise HarvesterError('Invalid CJ response')
         try:
@@ -31,6 +28,16 @@ def fetch_merchants(params=None):
         except (TypeError, ValueError):
             raise HarvesterError('Invalid CJ response: total_matched')
 
-        merchants.extend(response.get('advertiser', []))
+        data.extend(response.get(item, []))
 
-    return merchants
+    return data
+
+
+def fetch_merchants(params=None):
+    params = (isinstance(params, dict) and params) or {'keywords': 'video'}
+    return fetch_cj_data(params, 'https://advertiser-lookup.api.cj.com/v3/advertiser-lookup/', 'advertiser')
+
+def fetch_deals(params=None):
+    params = (isinstance(params, dict) and params) or {'advertiser-ids': 3634487}
+    params['website-id'] = params.get('website-id', CJ_WEB_ID)
+    return fetch_cj_data(params, 'https://linksearch.api.cj.com/v2/link-search/', 'link')
